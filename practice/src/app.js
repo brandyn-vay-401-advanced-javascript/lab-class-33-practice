0307607170bbdd61f4af424998734f861018e5bb
Brandyn Vay <brandynvay@protonmail.com>
2019-09-25 20:15:57 -0700

    done

--- practice/src/app.js
+++ practice/src/app.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Counter from './components/counter/counter.js';
+import CounterContext from "./components/context/counter-context.js";
+
+import Counter from "./components/counter/counter.js";
+import Increment from "./components/counter/increment.js";
+import Decrement from "./components/counter/decrement.js";
+
+import "./components/counter/counter.scss";
 
 export default class App extends React.Component {
   render() {
     return (
-      <>
+      <CounterContext>
+        <Increment />
         <Counter />
-      </>
+        <Decrement />
+      </CounterContext>
     );
   }
 }
